62a125eeeb4dce6e1d8b8afe75efd4e93d82480b
ftiet <ftiet21@gmail.com>
2018-10-20 17:17:07 -0400

    Take in user GET call

--- app/__init__.py
+++ app/__init__.py
@@ -14,23 +14,23 @@ app.config.from_mapping(
 @app.route('/restaurants', methods=['GET'])
 def restaurants():
     if request.method == 'GET':
-        json_data = request.get_json()
-        if not json_data:
+        data = request.get_json()
+        if not data:
             return jsonify({'message': 'No input data'}), 400
 
-        # coords = get_gmaps_coordinates(json_data[])
-        coords = get_gmaps_coordinates('Vanderbilt University, Nashville', 'Klaus Advanced Computing Center, Atlanta', (time.time(), time.time()+3600))
+        coords = get_gmaps_coordinates(data['startLocation'], data['endLocation'], (data['startInterval'], data['endInterval']), data['startTime'])
+        # coords = get_gmaps_coordinates('Vanderbilt University, Nashville', 'Klaus Advanced Computing Center, Atlanta', (time.time(), time.time()+3600))
         restaurants = get_yelp(coords)
         return jsonify(restaurants), 200
 
 @app.route('/directions', methods=['GET'])
 def directions():
     if request.method == 'GET':
-        json_data = request.get_json()
-        if not json_data:
+        data = request.get_json()
+        if not data:
             return jsonify({'message': 'No input data'}), 400
 
-        # directions = get_gmaps_directions(json_data[])
+        # directions = get_gmaps_directions(data[])
         directions = get_gmaps_directions('Vanderbilt University, Nashville', 'Klaus Advanced Computing Center, Atlanta')
         return jsonify(directions), 200
 
